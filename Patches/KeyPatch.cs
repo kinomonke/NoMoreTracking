e008fbe5e7e2e383dfb921a12f002882df262abd
kinomonke <kinogtag@outlook.com>
2026-07-12 14:06:42 +0100

    V1.00

---
+++ Patches/KeyPatch.cs
@@ -0,0 +1,14 @@
+﻿using GorillaNetworking;
+using HarmonyLib;
+
+namespace NoMoreTracking.Patches;
+
+[HarmonyPatch(typeof(GorillaKeyboardButton), "OnButtonPressedEvent")]
+class KeyPatch
+{
+    static void Postfix(GorillaKeyboardButton __instance)
+    {
+        if (__instance.Binding == GorillaKeyboardBindings.option3 && Plugin.CorrectTab)
+            Plugin.JoinRandomString();
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using GorillaNetworking;
+using HarmonyLib;
+using System;
+using System.Linq;
+
+namespace NoMoreTracking;
+
+[BepInPlugin(Constants.GUID, Constants.NAME, Constants.VERS)]
+public class Plugin : BaseUnityPlugin
+{
+    static readonly Random Random = new();
+
+    void Start() =>
+        Harmony.CreateAndPatchAll(typeof(Plugin).Assembly, Constants.GUID);
+
+    public static bool CorrectTab =>
+        GorillaComputer.instance.currentState == GorillaComputer.ComputerState.Room;
+
+    public static void JoinRandomString()
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        PhotonNetworkController.Instance.AttemptToJoinSpecificRoom(
+            new string([.. Enumerable.Range(0, 6).Select(_ => chars[Random.Next(chars.Length)])]),
+            JoinType.Solo);
+    }
+}
+
+class Constants
+{
+    public const string GUID = "kinomonke.nomoretracking",
+        NAME = "NoMoreTracking",
+        VERS = "1.00";
+}
